c4bd17c2216af74d5d49397bcdc485e3263a1e82
yonghai.yang <yangyonghai@wusongtech.com>
2018-10-11 17:31:55 +0800

    form :banana:

--- control/todo.js
+++ control/todo.js
@@ -3,9 +3,22 @@
  */
 angular.module('todoApp', [])
     .controller('TodoListController', function () {
-        this.click = function () {
-            alert([1,2])
+        var todoList = this;
+        todoList.todos = [
+            {text:'learn AngularJS', done:true},
+            {text:'go to Alibaba', done:true},
+        ];
+
+
+        // todoList.todoText = function (e) {
+        //     console.log(e);
+        // };
+        todoList.addTodo = function () {
+            console.log(todoList.todoText);
+            todoList.todos.push({text: todoList.todoText, done: false});
+            todoList.todoText = '';
         }
+
 
 
     });
