498ebc27f12d0b5def43b3d672d5beb39e0025d0
yonghai.yang <yangyonghai@wusongtech.com>
2018-10-11 15:37:47 +0800

    test :smile:

---
+++ control/todo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yang on 2018/10/11.
+ */
+angular.module('todoApp', [])
+    .controller('TodoListController', function () {
+        this.click = function () {
+            alert([1,2])
+        }
+
+
+    });
