f4e467ef55f292df3cab35d13d89b5daded8892f
yonghai.yang <yangyonghai@wusongtech.com>
2018-10-12 14:13:58 +0800

    用ES6方法

--- control/todo.js
+++ control/todo.js
@@ -2,23 +2,27 @@
  * Created by yang on 2018/10/11.
  */
 angular.module('todoApp', [])
-    .controller('TodoListController', function () {
+    .controller('TodoListController', function() {
         var todoList = this;
         todoList.todos = [
             {text:'learn AngularJS', done:true},
-            {text:'go to Alibaba', done:true},
-        ];
+            {text:'build an AngularJS app', done:false}];
 
-
-        // todoList.todoText = function (e) {
-        //     console.log(e);
-        // };
-        todoList.addTodo = function () {
+        todoList.addTodo = function() {
             console.log(todoList.todoText);
-            todoList.todos.push({text: todoList.todoText, done: false});
+            todoList.todos.push({text:todoList.todoText, done:false});
             todoList.todoText = '';
-        }
+        };
 
+        todoList.remaining = function() {
+            return todoList.todos.filter((item) => {
+                return item.done === false;
+            }).length;
+        };
 
-
+        todoList.archive = function() {
+            todoList.todos = todoList.todos.filter((item) => {
+                return item.done === false;
+            })
+        };
     });
